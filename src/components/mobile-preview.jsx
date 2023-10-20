a819e302997c74b012f624e2a0b97599e388ee7e
wmerfalen <dev@bnull.net>
2023-10-20 11:04:36 -0600

    fix: turned background black on mobile to fix really bright background image

--- src/components/mobile-preview.jsx
+++ src/components/mobile-preview.jsx
@@ -12,7 +12,19 @@ const element = <FontAwesomeIcon icon={faCoffee} />;
 
 function MobilePreview() {
   const [show, setShow] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
+    var element = document.getElementById("canary-value"),
+      style = window.getComputedStyle(element),
+      top = style.getPropertyValue("display");
+    console.log(top);
+    if (top === "none") {
+      setShow(true);
+      setIsMobile(true);
+      return;
+    }
+    setIsMobile(false);
+
     if (!localStorage.getItem("showMP")) {
       localStorage.setItem("showMP", true);
     }
@@ -29,29 +41,31 @@ function MobilePreview() {
   return (
     <>
       <div className={`MobilePreview`}>
-        {!show && 
-                <svg
-                  className="expand-toggle-button"
-                  height="1em"
-                  viewBox="0 0 512 512"
-                  onClick={expand}
-                >
-                  <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160zm352-160l-160 160c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0z" />
-                </svg>
-        }
+        <span id="canary-value"></span>
+        {!show && (
+          <svg
+            className="expand-toggle-button"
+            height="1em"
+            viewBox="0 0 512 512"
+            onClick={expand}
+            id="image_1"
+          >
+            <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160zm352-160l-160 160c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0z" />
+          </svg>
+        )}
+        {show && (
+          <svg
+            className="collapse-toggle-button"
+            height="1em"
+            viewBox="0 0 512 512"
+            onClick={collapse}
+          >
+            <path d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z" />
+          </svg>
+        )}
         <div className={`right-nav ${show ? "" : "MobilePreviewHide"}`}>
           <div className="mobile-preview">
             <div className="place-horizontal">
-              {show && (
-                <svg
-                  className="toggle-button"
-                  height="1em"
-                  viewBox="0 0 512 512"
-                  onClick={collapse}
-                >
-                  <path d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z" />
-                </svg>
-              )}
               <h2 className="section-title">Mobile Preview</h2>
             </div>
             <p>
